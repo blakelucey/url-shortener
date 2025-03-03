fb56a8759ff4b15a3736c51083f0a5ed6c18aaf8
Blake Lucey <blucey7@gmail.com>
2025-03-03 15:31:33 -0600

    feat(ui): new link ux updates

--- src/components/create-link-input.tsx
+++ src/components/create-link-input.tsx
@@ -4,6 +4,19 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button"; // Assuming this is available in your UI library
 import axios from 'axios'
 import { useAppKitAccount } from '@reown/appkit/react';
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { ChannelComboxInput } from './channel-input';
+import { nanoid } from "nanoid";
+import Link from 'next/link';
 
 const CreateLinkInput = () => {
     const [link, setLink] = useState("");
@@ -11,6 +24,7 @@ const CreateLinkInput = () => {
     const [isSubmitting, setIsSubmitting] = useState(false);
 
     const userId = caipAddress;
+    const shortHash = nanoid(7);
 
     const handleSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
@@ -30,28 +44,50 @@ const CreateLinkInput = () => {
         }
     };
 
+
     return (
-        <form onSubmit={handleSubmit} className="space-y-4 max-w-sm mx-auto">
-            <div className="space-y-1.5">
-                <Label htmlFor="link" className="text-gray-700">Input your link here:</Label>
-                <Input
-                    type="url"
-                    id="link"
-                    placeholder="https://example.com"
-                    value={link}
-                    onChange={(e) => setLink(e.target.value)}
-                    className="w-full"
-                    required
-                />
-            </div>
-            <Button type="submit" className="w-full">Shorten URL</Button>
-            {link && (
-                <div className="mt-4">
-                    <p className="text-sm text-gray-500">Preview:</p>
-                    <p className="text-blue-500 break-all">{link}</p>
-                </div>
-            )}
-        </form>
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="outline">Add a new link</Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Add your link</SheetTitle>
+                    <SheetDescription>
+                        Add new links here. Add your link, see what it will look like, input channels and campaigns for analytics. Click submit when you're done.
+                    </SheetDescription>
+                </SheetHeader>
+                <form onSubmit={handleSubmit} className="space-y-4 max-w-sm mx-auto">
+                    <div className="space-y-1.5">
+                        <Label htmlFor="link" className="text-gray-700">Input your link here:</Label>
+                        <Input
+                            type="url"
+                            id="link"
+                            placeholder="https://example.com"
+                            value={link}
+                            onChange={(e) => setLink(e.target.value)}
+                            className="w-full"
+                            required
+                        />
+                    </div>
+                    {link && (
+                        <div className="mt-4">
+                            <p className="text-sm text-gray-500">Preview:</p>
+                            <p className="text-blue-500 break-all"><Link href={link} target='a'>`short.ly/{shortHash}`</Link></p>
+                        </div>
+                    )}
+                    <div className='space-y-1.5'>
+                        <Label htmlFor="What channels will you use?" className='text-gray-700'>What channels will you use?</Label>
+                        <ChannelComboxInput />
+                    </div>
+                    <SheetFooter>
+                        <SheetClose asChild>
+                            <Button type="submit" className="w-full">Shorten URL</Button>
+                        </SheetClose>
+                    </SheetFooter>
+                </form>
+            </SheetContent>
+        </Sheet>
     );
 };
 
