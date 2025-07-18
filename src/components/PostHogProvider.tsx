eafec48bd7add7d9af31d932968420ea0ce3a6f9
Blake M Lucey <64055920+blakelucey@users.noreply.github.com>
2025-07-18 11:15:50 -0500

    feat(posthog): update to posthog configuration (#33)

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -4,15 +4,29 @@ import posthog from "posthog-js"
 import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react"
 import { Suspense, useEffect } from "react"
 import { usePathname, useSearchParams } from "next/navigation"
+import { logFn } from "../../logging/logging"
+const log = logFn("src.components.PostHogProvider.tsx.")
+
+
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  log("checkingWindow", "debug", {
+    typeofWindow: typeof window,
+    key: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
+    host: process.env.NEXT_PUBLIC_POSTHOG_KEY!,
+  });
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      ui_host: "https://us.posthog.com",
-      capture_pageview: false, // We capture pageviews manually
-      capture_pageleave: true, // Enable pageleave capture
-      debug: process.env.NODE_ENV === "development",
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      capture_pageview: true,
+      capture_pageleave: true,
+      debug: process.env.NEXT_PUBLIC_STAGE! === "development" ? true : false,
+      capture_dead_clicks: true,
+      capture_heatmaps: true,
+      capture_performance: true,
+      capture_exceptions: true,
+      autocapture: true,
+      person_profiles: "identified_only"
     })
   }, [])
 
